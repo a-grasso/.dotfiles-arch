a39e690b4b42ac0970e6109eae5416264da243eb
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-13 23:52:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,9 @@ ssh-add
 
 # dotfiles setup
 echo "dotfiles"
-#sudo pacman -S --noconfirm git
+
+sudo pacman -S --noconfirm git
+
 #mkdir $HOME/.dotfiles
 #git init --bare $HOME/.dotfiles
 #alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
