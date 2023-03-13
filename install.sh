07e94a333e46ea65ef36c3813af785b244514271
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-13 22:36:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,23 +2,28 @@
 
 set -e
 
-# bitwarden cli
+# bitwarden cli & ssh key prep
+echo "ssh prep"
+
 sudo pacman -S --noconfirm bitwarden-cli
 
-## ssh key prep
 bw login
 # -> email
 # -> master password
 
+export BW_SESSION=$(bw unlock --raw)
+
 bw sync
 
 mkdir ~/.ssh
-chmod 0700 ~/.ssh
+sudo chmod 0700 ~/.ssh
 
 bw get notes pub > ~/.ssh/id_rsa.pub
 bw get notes sshkey > ~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa
+sudo chmod 0600 ~/.ssh/id_rsa
 
+# ssh
+echo "ssh"
 ssh-agent
 
 eval $(ssh-agent)
@@ -26,6 +31,7 @@ eval $(ssh-agent)
 ssh-add
 
 # dotfiles setup
+echo "dotfiles"
 
 sudo pacman -S --noconfirm git
 
@@ -40,6 +46,7 @@ dotfiles config --local status.showUntrackedFiles no
 dotfiles remote add origin git@github.com:a-grasso/.dotfiles-arch.git
 
 # zsh & oh-my-zsh
+echo "zsh"
 
 sudo pacman -S --noconfirm zsh
 
@@ -48,5 +55,10 @@ chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # theme
-
+echo "theme"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# apply config
+echo "applying dotfiles configs"
+
+echo "DONE"
