5b32f207131af05be615b13eee36f77850688585
Antonino <antonino.grasso.it@gmail.com>
2023-03-14 13:26:15 +0100

    update

---
+++ install-cz.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# git
+echo "-----> git"
+
+sudo pacman -S --noconfirm git
+
+# zsh & oh-my-zsh
+echo "-----> zsh & oh-my-zsh"
+
+sudo pacman -S --noconfirm zsh
+
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# theme
+echo "theme"
+git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k # $ZSH_CUSTOM would need a change into zsh shell
+
+# apply config
+echo "-----> chezmoi & applying dotfiles"
+
+sudo pacman -S --noconfirm chezmoi
+
+chezmoi init --apply --verbose https://github.com/a-grasso/.dotfiles-arch.git
+
+# bitwarden cli
+echo "-----> bitwarden cli"
+
+sudo pacman -S --noconfirm bitwarden-cli
+
+export BW_SESSION=$(bw login --raw)
+# -> input email
+# -> input master password
+
+bw sync
+
+#mkdir ~/.ssh
+#sudo chmod 0700 ~/.ssh
+
+#bw get notes pub > ~/.ssh/id_rsa.pub
+#bw get notes sshkey > ~/.ssh/id_rsa
+#sudo chmod 0600 ~/.ssh/id_rsa
+
+# ssh
+echo "-----> ssh"
+ssh-agent
+
+eval $(ssh-agent)
+
+ssh-add
+
+echo "DONE"
+
+sleep 5
+sudo reboot
