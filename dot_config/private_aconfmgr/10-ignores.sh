44c267bf22e4469c669e73108847e7127830f266
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-18 00:01:54 +0100

    Update 10-ignores.sh

--- dot_config/private_aconfmgr/10-ignores.sh
+++ dot_config/private_aconfmgr/10-ignores.sh
@@ -4,3 +4,8 @@ IgnorePath '/var/lib/pacman/sync/*.db.sig'
 IgnorePath '/var/*'
 IgnorePath '/boot/*'
 IgnorePath '/usr/lib/*'
+
+white_list_etc=(
+    'lightdm/lightdm.conf'
+)
+IgnorePathsExcept /etc/ "${white_list_etc[@]}"
