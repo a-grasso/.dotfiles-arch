9ddacbfedf441155dbbdfe25c09a23dde7f4d50f
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-17 23:15:29 +0100

    Update install-cz.sh

--- install-cz.sh
+++ install-cz.sh
@@ -2,11 +2,12 @@
 
 sudo pacman -Sy
 
+############################################################
 # git
 echo "-----> git"
 
-sudo pacman -S --noconfirm git
-
+sudo pacman -S --noconfirm base-devel git
+############################################################
 # zsh & oh-my-zsh
 echo "-----> zsh & oh-my-zsh"
 
@@ -15,11 +16,11 @@ sudo pacman -S --noconfirm zsh
 chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
+############################################################
 # theme
 echo "-----> theme"
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k # $ZSH_CUSTOM would need a change into zsh shell
-
+############################################################
 # bitwarden cli
 echo "-----> bitwarden cli"
 
@@ -30,7 +31,12 @@ export BW_SESSION=$(bw login --raw)
 # -> input master password
 
 bw sync
+############################################################
+# lightdm
+sudo pacman -S --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings 
 
+sudo systemctl enable lightdm.service -f
+############################################################
 # apply config
 echo "-----> chezmoi & applying dotfiles"
 
@@ -44,7 +50,7 @@ chezmoi init --apply --verbose https://github.com/a-grasso/.dotfiles-arch.git
 #bw get notes pub > ~/.ssh/id_rsa.pub
 #bw get notes sshkey > ~/.ssh/id_rsa
 #sudo chmod 0600 ~/.ssh/id_rsa
-
+############################################################
 # ssh
 echo "-----> ssh"
 ssh-agent
@@ -52,7 +58,18 @@ ssh-agent
 eval $(ssh-agent)
 
 ssh-add
+############################################################
+# aconfmgr
+if pacman -Qs yay > /dev/null ; then
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+fi
 
+yay -S --noconfirm aconfmgr
+
+aconfmgr apply
+############################################################
 echo "DONE"
 
 sleep 5
