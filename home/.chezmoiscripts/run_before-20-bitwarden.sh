73db5044e6815348a3e931428490d1230bd84392
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-18 21:38:26 +0100

    Update run_before-20-bitwarden.sh

--- home/.chezmoiscripts/run_before-20-bitwarden.sh
+++ home/.chezmoiscripts/run_before-20-bitwarden.sh
@@ -2,4 +2,7 @@
 
 # Bitwarden session
 sudo -u "$(whoami)" zsh -c "export BW_SESSION=$(bw login --raw)"
+sudo -u "$(whoami)" sh -c "export BW_SESSION=$(bw login --raw)"
+sudo -u "$(whoami)" bash -c "export BW_SESSION=$(bw login --raw)"
+
 bw sync
