e1f03df27037603cb90910dba3c5ed3abe1d8f3a
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-18 23:17:07 +0100

    Update run_before-20-bitwarden.sh

--- home/.chezmoiscripts/run_before-20-bitwarden.sh
+++ home/.chezmoiscripts/run_before-20-bitwarden.sh
@@ -3,7 +3,8 @@
 # Bitwarden session
 #"$(echo $0)" -c "export BW_SESSION=$(bw login --raw)"
 
-export BW_SESSION=$(bw login --raw)
+export BW_SESSION=$(bw {login,unlock} --raw)
+
 echo $0
 env
 bw sync
