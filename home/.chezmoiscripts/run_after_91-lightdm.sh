9ffa404516197d49305c65fc30af5b636c49a6fb
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-19 15:48:14 +0100

    Update run_after_91-lightdm.sh

--- home/.chezmoiscripts/run_after_91-lightdm.sh
+++ home/.chezmoiscripts/run_after_91-lightdm.sh
@@ -15,4 +15,4 @@ trap sudo_finish EXIT
 
 sudo -u "$(whoami)" -n bash -c "command -v lightdm"
 echo "---"
-sudo -u "$(whoami)" -n bash -c "sudo systemctl enable ligthdm"
+sudo -u "$(whoami)" -n bash -c "sudo systemctl enable lightdm"
