a6576155d87d412391c4828a61b20af60c33606a
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-19 17:19:53 +0100

    Create run_once_after_90-final.sh

---
+++ home/.chezmoiscripts/run_once_after_90-final.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+
+echo "---All DONE---"
+
+yesno() {
+	read -p "${*} [y/n] " -n 2 -r
+	printf "\n"
+	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+		exit 0
+	fi
+}
+
+yesno "Do you want to reboot now?"
+
+echo "rebooting..."
+
+sleep 3
+
+reboot
