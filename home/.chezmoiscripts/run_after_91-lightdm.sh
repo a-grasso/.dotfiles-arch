0af07045c1020bfc564fd4e77ae084b50811c650
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-19 15:45:47 +0100

    Update and rename run_once_after_91-lightdm.sh to run_after_91-lightdm.sh

---
+++ home/.chezmoiscripts/run_after_91-lightdm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+su root -c "echo \"Create temporary permissions for sudo...\" && \
+mv -f /etc/sudoers /etc/sudoers.bak && \
+echo \"root ALL=(ALL) ALL\" > /etc/sudoers && \
+echo \"$(whoami) ALL = NOPASSWD : ALL\" >> /etc/sudoers && \
+echo \"%wheel ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers
+"
+
+function sudo_finish {
+	echo "Restore permissions for sudo"
+	sudo mv -f /etc/sudoers.bak /etc/sudoers
+}
+trap sudo_finish EXIT
+
+sudo -u "$(whoami)" -n bash -c "systemctl enable ligthdm"
