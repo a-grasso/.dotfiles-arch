ea4fd300a4d43e8ee78f4b35824ecdab62c15006
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-19 17:19:14 +0100

    Update run_once_after_80-closedown.sh

--- home/.chezmoiscripts/run_once_after_80-closedown.sh
+++ home/.chezmoiscripts/run_once_after_80-closedown.sh
@@ -2,6 +2,8 @@
 
 clear
 
+echo "---CLOSEDOWN---
+
 su root -c "echo \"Create temporary permissions for sudo...\" && \
 mv -f /etc/sudoers /etc/sudoers.bak && \
 echo \"root ALL=(ALL) ALL\" > /etc/sudoers && \
@@ -28,24 +30,3 @@ ssh-add
 
 ############## lightdm
 sudo -u "$(whoami)" -n bash -c "sudo systemctl enable lightdm"
-
-############## after all
-clear
-
-echo "---All DONE---"
-
-yesno() {
-	read -p "${*} [y/n] " -n 2 -r
-	printf "\n"
-	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-		exit 0
-	fi
-}
-
-yesno "Do you want to reboot now?"
-
-echo "rebooting..."
-
-sleep 3
-
-reboot
