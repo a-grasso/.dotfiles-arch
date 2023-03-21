13ccf779806c896b2584ed7f3bf46805e72b61a1
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-21 21:22:19 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
+
+source ./commin.sh
+
+pacman -Sy
+
+# sudo is needed for dotfiles
+checkDep 'sudo' 'command -v sudo' 'pacman -S --noconfirm sudo'
+
+# git is needed for dotfiles
+checkDep 'git' 'command -v git' 'sudo pacman -S --noconfirm git'
+
+# clone this very repo
+git clone "${dotfiles}" repo
+cd repo
+sudo chmod +x setup.sh
+
+# setup everything
+./setup.sh
