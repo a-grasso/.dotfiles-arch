61543b1675338f46da628b4bc2521d1c58004f1d
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-23 22:32:50 +0100

    Update run_once_after_80-closedown.sh

--- home/.chezmoiscripts/run_once_after_80-closedown.sh
+++ home/.chezmoiscripts/run_once_after_80-closedown.sh
@@ -2,6 +2,13 @@
 
 echo "---CLOSEDOWN---"
 
+su root -c "echo \"Create temporary permissions for sudo...\" && \
+mv -f /etc/sudoers /etc/sudoers.bak && \
+echo \"root ALL=(ALL) ALL\" > /etc/sudoers && \
+echo \"$(whoami) ALL = NOPASSWD : ALL\" >> /etc/sudoers && \
+echo \"%wheel ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers
+"
+
 function sudo_finish {
 	echo "Restore permissions for sudo"
 	sudo mv -f /etc/sudoers.bak /etc/sudoers
@@ -22,4 +29,5 @@ ssh-add
 
 ############## lightdm
 
+sudo -u "$(whoami)" -n bash -c "sudo systemctl enable lightdm"
 sudo systemctl enable lightdm
