c3be49b50d385517436806d373cad4d51d7098dd
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-25 15:47:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "---ENTRYPOINT---"
-
 set -o errexit  # abort on nonzero exitstatus
 set -o nounset  # abort on unbound variable
 set -o pipefail # don't hide errors within pipes
@@ -10,35 +8,22 @@ readonly dotfiles='https://github.com/a-grasso/.dotfiles-arch'
 
 # sudo is needed for dotfiles
 if ! command -v sudo -p &>/dev/null; then
-		pacman -Sy --noconfirm sudo > /dev/null
+		pacman -Sy --noconfirm sudo &> /dev/null
 fi
 
-sudo pacman -Sy > /dev/null
+sudo pacman -Sy --noconfirm --needed sudo git gum &> /dev/null
 
-# git is needed for dotfiles
-if ! command -v git -p &>/dev/null; then
-		sudo pacman -S --noconfirm git > /dev/null
-fi
+gum style \
+	--border double \
+	--align center \
+	"ENTRYPOINT"
 
-# gum is needed for further scripting
-if ! command -v gum -p &>/dev/null; then
-		sudo pacman -S --noconfirm gum  > /dev/null
-fi
-
-# need a scratch space for downloading files
-tmpDir="$(mktemp -d -t dotfiles-repo-XXXXXXXXXX)"
-if [[ ! -d "${tmpDir}" ]]; then
-	printf "${red}!!! %s${reset}\n" "Failed creating a temporary directory; cannot continue" 1>&2
-	exit 1
-fi
-function cleanup {
-	echo "Cleaning up entrypoint"
-	sudo rm -rf ${tmpDir}
-}
-trap cleanup EXIT
+# scratch work directory
+tmpDir="$(mktemp -d -t entrypoint-XXXX)"
+trap "sudo rm -rf ${tmpDir}" EXIT
 
 # clone this very repo
-git clone --quiet "${dotfiles}" "${tmpDir}" > /dev/null
+git clone --quiet "${dotfiles}" "${tmpDir}"
 cd "${tmpDir}"
 
 # setup everything
