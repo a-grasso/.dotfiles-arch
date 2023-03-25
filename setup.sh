e457d5b59d2a1137096aec55f9b853c58bd8e64b
Antonino Grasso <56027046+a-grasso@users.noreply.github.com>
2023-03-25 15:56:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-echo "---SETUP---"
-
 set -o errexit  # abort on nonzero exitstatus
 set -o nounset  # abort on unbound variable
 set -o pipefail # don't hide errors within pipes
 
 source ./common.sh
+
+gum style \
+	--border double \
+	--align center \
+	"SETUP"
+
 
 # need a scratch space for downloading files
 #tmpDir="$(mktemp -d -t dev-setup-XXXXXXXXXX)"
