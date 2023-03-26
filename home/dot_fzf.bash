fc25ffef78386fad1c567c292fc3b9f3cca5e307
Antonino Grasso <antonino.grasso.it@gmail.com>
2023-03-26 11:57:09 +0000

    add fzf shell config

---
+++ home/dot_fzf.bash
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/anto/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/anto/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/anto/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/anto/.fzf/shell/key-bindings.bash"
